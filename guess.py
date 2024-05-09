db943bc15356ad2f839efc3dd27db68f605f7e6d
cecvl <christiancecil.dev@gmail.com>
2024-05-09 20:29:36 +0300

    strings & loops

---
+++ guess.py
@@ -0,0 +1,9 @@
+guess  = 0
+
+while guess != 5:
+    guess = int(input("Guess a number between 1 and 10: "))
+print("You guessed it!")
+
+for i  in range(2):
+    print("I will commmit regularly")
+
---
+++ strings.py
@@ -0,0 +1,7 @@
+# string concatenation
+# loop backwards
+
+for i in range (1,10):
+    print(f'{i}  bottles of beer on the wall')
+    print(f'{i}  bottles of beer')
+    print('Take one down, pass it around')
