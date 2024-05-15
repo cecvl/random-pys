72ce38cdda8b59aa7a030e185b3d4ba6dfe7269a
cecvl <christiancecil.dev@gmail.com>
2024-05-15 11:59:18 +0300

    import list & classes into main module

--- modules/main.py
+++ modules/main.py
@@ -5,7 +5,21 @@ import datetime, bday_messages
 # A: today = datetime.date.today()
 # year, month, day
 today = datetime.date.today()
-birthday = datetime.date(2004, 5, 1)
+print(today)
+
+birthday = datetime.date(2005, 4, 5)
+print(birthday)
 
 age = today.year - birthday.year
 print(age)
+
+# calculate days away from next birthday
+next_birthday = datetime.date(today.year, birthday.month, birthday.day)
+print(next_birthday)
+
+days_away = (next_birthday - today).days
+
+if today == next_birthday:
+    print("Happy Birthday!")
+else:
+    print(f"{(days_away)} days until your birthday!")
