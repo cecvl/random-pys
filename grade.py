a9c490b168542c4d661b4d170b9dcd11f39c2a1f
cecvl <christiancecil.dev@gmail.com>
2024-05-19 23:15:57 +0300

    update
    using lists

--- grade.py
+++ grade.py
@@ -1,5 +1,10 @@
-grade = 90
-if grade < 55:
+grades = [50,50,66,68,50]
+sum  = 0
+for i in grades:
+    sum += i
+average = sum/5
+print(average)
+if average < 55:
     print("Fail")
 else:
     print("Pass")
